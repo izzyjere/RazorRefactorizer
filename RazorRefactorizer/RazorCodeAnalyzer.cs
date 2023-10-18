b5f6d328987cc2f6102d401b7b90cfa1a1ee5f89
izzyjere <wisdomjere5@gmail.com>
2023-10-18 09:28:48 +0200

    Add project files.

---
+++ RazorRefactorizer/RazorCodeAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis.Text;
+
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RazorRefactorizer
+{
+    public class RazorCodeAnalyzer
+    {
+        public List<RazorCodeSelection> AnalyzeRazorCode(string razorCode)
+        {
+            var razorCodeSelections = new List<RazorCodeSelection>();
+
+            // Finding all the sections of markup within Razor component
+            var matches = Regex.Matches(razorCode, @"<[^>]+>([^<]*)</[^>]+>");
+
+            foreach (Match match in matches)
+            {
+                var textSpan = new TextSpan(match.Index, match.Length);
+                var selectedCode = match.Value;
+                razorCodeSelections.Add(new RazorCodeSelection
+                {
+                    Span = textSpan,
+                    SelectedCode = selectedCode
+                });
+            }
+
+            return razorCodeSelections;
+        }
+    }
+}
---
+++ RazorRefactorizer/RazorCodeSelection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis.Text;
+
+namespace RazorRefactorizer
+{
+    public class RazorCodeSelection
+    {
+        public TextSpan Span { get; set; }
+        public string SelectedCode { get; set; }
+    }
+}
